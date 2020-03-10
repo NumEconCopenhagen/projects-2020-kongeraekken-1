2ed9b621553ce81ecf307e71fc3cc18cec476886
FrederikDegn <FrederikDegn@users.noreply.github.com>
2020-03-10 23:16:53 +0100

    d

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -27,14 +27,15 @@ def u_func(l,eps,kappa,nu,m,tau0,tau1,w):
 def u_optimizer(eps,kappa,nu,m,tau0,tau1,w):
     """This function optimizes u_func wrt labor input. It prints out the optimal labor supply,
     the implied optimal consumption and the utility derived from this optimal combination"""
-    
-    obj = -u_func(l=l,eps,kappa,nu,m,tau0,tau1,w)
 
-    sol = optimize.minimize_scalar(u_func,method="bounded",
+    def objective(l,eps,kappa,nu,m,tau0,tau1,w):
+        return -u_func(l=l,eps=eps,kappa=kappa,nu=nu,m=m,tau0=tau0,tau1=tau1,w=w)
+
+    sol = optimize.minimize_scalar(objective,method="bounded",
     bounds=(0,1),args=(eps,kappa,nu,m,tau0,tau1,w))
 
     l_star = sol.x
-    c_star = m + w*l - (tau0*w*l + tau1*np.max(w*l - kappa,0))
+    c_star = m + w*l_star - (tau0*w*l_star + tau1*np.max(w*l_star - kappa,0))
     u_star = u_func(l=l_star,eps=eps,kappa=kappa,nu=nu,m=m,tau0=tau0,tau1=tau1,w=w)
     return l_star, c_star, u_star
 
