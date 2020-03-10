042767e71550d8aeb16f7d43cc92fdd2d423ca2b
Christian <Christian@Christians-MacBook-Pro-2.local>
2020-03-10 17:21:22 +0100

    Updated with function in module

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,24 @@
-def square(x):
-    """ square numpy array
+import numpy as np
+from scipy import optimize
+import matplotlib.pyplot as plt
+
+def u_func(l,eps,kappa,nu,m,tau0,tau1,w):
+    """
+    CRRA utility function for an rational agent making a consumption and labour decision subject
+    to constraints. The optimisation problem only considers labour as consumption is an implicit
+    function hereof. 
     
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
+    Variables:
+    l: labour
+    eps: Frisch elasticity of labour supply
+    kappa: cut-off for top labour income bracket
+    nu: a scalar for the weight put on disutility of labour
+    m: cash-on-hand
+    tau0: normal tax rate
+    tau1: top labour income bracket tax rate
+    w: wage rate
     
     """
     
-    y = x**2
-    return y
+    u = np.log(m + w*l - (tau0*w*l + tau1*np.max(w*l - kappa,0))) - nu*(l**(1+1/eps)/(1+1/eps))
+    return u
