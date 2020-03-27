fb8a455e9a9305b45ef0d9138506eaa39ab05489
FrederikDegn <FrederikDegn@users.noreply.github.com>
2020-03-27 09:15:38 +0100

    Initial cleaning

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,3 +1,38 @@
+# Importing packages
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import pandas as pd
+#import pydst
+#dst = pydst.Dst(lang='en')
+
+#setting up work directory
+import os 
+assert os.path.isdir('dataproject/')
+assert os.path.isfile('dataproject/RAS200.xlsx')
+assert os.path.isfile('dataproject/INDKP101.xlsx')
+os.listdir('dataproject/')
+
+#cleaning data
+empl_data = 'dataproject/RAS200.xlsx'
+inc_data = 'dataproject/INDKP101.xlsx'
+
+for var in empl, inc:
+
+    var = pd.read_excel(var +"_data"), skiprows=2)
+    drop_unnamed = ['Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3']
+    var.drop(drop_unnamed, axis=1, inplace=True)
+    var.rename(columns = {'Unnamed: 4':'municipality'}, inplace=True)
+    var.rename(columns = {str(i): f'var{i}' for i in range(2004,2018)}, inplace=True)
+    var.rename(columns = myDict, inplace=True)
+    var+"_long" = pd.wide_to_long(var, stubnames= var, i='municipality', j='year')
+
+next
+
+
+
+
+
 def only_keep_municipalities(df):
     """ delete all non-municipalities
 
