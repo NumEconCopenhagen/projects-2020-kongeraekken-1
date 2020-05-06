5302a0a1542ace2db33af5b4134adfb677f69580
ChristianBrauerJohanssen <60613177+ChristianBrauerJohanssen@users.noreply.github.com>
2020-05-06 11:43:53 +0200

    final untitled update

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -5,10 +5,11 @@ def solve_for_consumption(m1,r,beta,rho):
 
     Args:
         m1 (float): period 1 cash on hand
-        g (float): technological growth rate
-        n (float): population growth rate
-        alpha (float): cobb-douglas parameter
-        delta (float): capital depreciation rate 
+        r (float): real interest rate
+        beta (float): subjective discount rate (patience)
+        alpha (float): coefficient of relative risk aversion
+            (the inverse is the elasticity of intertemporal 
+             substitution)
 
     Returns:
         result (RootResults): the solution represented as a RootResults object
@@ -16,10 +17,13 @@ def solve_for_consumption(m1,r,beta,rho):
     """ 
     
     # a. define objective function
-    f = lambda k: k**alpha
-    obj_kss = lambda kss: kss - (s*f(kss) + (1-delta)*kss)/((1+g)*(1+n))
+    c1_rootform = lambda c1: c1 - ((1+r)*(m1-c1)/(beta*(1+r))**(1/rho))
 
-    #. b. call root finder
-    result = optimize.root_scalar(obj_kss,bracket=[0.1,100],method='bisect')
-    
-    return result
+    # b. call root finder
+    result = optimize.root_scalar(c1_rootform,bracket=[0.1,100],method='brentq')
+    c1_s = result.root
+                                                           
+    # c. residual calculation of c2
+    c2_s = (1+r)*(m1-c1_s)
+                                                           
+    return c1_s, c2_s
